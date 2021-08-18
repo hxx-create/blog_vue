c8c736eb764a2a91911f16e4e399c48babba45dd
何祥 <846115027@qq.com>
2021-08-18 09:08:49 +0800

    asiox

---
+++ src/api/common.js
@@ -0,0 +1,26 @@
+//src/api/common.js
+import request from '@/utils/request'
+
+/**
+ * POST 方法 与 GET方法  区别
+ *
+ * post==> data: obj
+ * get==>params: obj
+ *
+ */
+// 例如(post)：
+ export function updateBook(obj) {
+   return request({
+     url: '/book/update',      // url = base url + request url
+     method: 'post',
+     data: obj,    //----------------->>>区别
+   })
+ }
+// 例如(get)：
+ export function getBookList(obj) {
+   return request({
+     url: '/book/list',      // url = base url + request url
+     method: 'post',
+     params: obj,    //----------------->>>区别
+   })
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 1.1导入路由的包
+import VueRouter from 'vue-router';
+// 1.2安装路由
+Vue.use(VueRouter);
+// 1.3 导入自己的 router.js 模块
+import router from './router/router.js';
 Vue.config.productionTip = false
 
 new Vue({
